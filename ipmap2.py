718a7f9bff1192197471329d86b71d37ce1c9277
akanshaaggarwal345 <36618578+akanshaaggarwal345@users.noreply.github.com>
2019-04-01 15:24:18 +0530

    Update ipmap2.py

--- ipmap2.py
+++ ipmap2.py
@@ -1,29 +1,27 @@
-
+# here we will collect all ipaddresses,their iplocations and their respective ipcounts and  store this information in the file ipcountandlocation.csv
 import sqlite3,csv
 def maptoloc():
 	flag=0
-	con=sqlite3.connect("dbip.db")
+	con=sqlite3.connect("dbip.db")#connect to dbip database that we have created
 	con.text_factory=str
 	cur=con.cursor()
 
 	cur.execute("DROP TABLE IF EXISTS iploc")
 	cur.execute("DROP TABLE IF EXISTS cache_table")
 
-	f1=open("ipcountandlocation.csv","w")
-	f=open("ipcount.csv","rU")
+	f1=open("ipcountandlocation.csv","w")#create new file ipcountandlocation.csv
+	f=open("ipcount.csv","rU")#read from file ipcount.csv written by genipcount.py
 	reader=csv.reader(f,delimiter=',')
 	for el in reader:
 		if(flag==0):
 			flag=1
 		
 		else:
-			ip=el[0]
-			ct=el[1]
-#ip=raw_input("enter IP ADDRESS: ")
-			intiplist=ip.split(".")
-	#print intiplist
-	#q=intiplist[0]
-	#print q
+			ip=el[0]#extract ip address
+			ct=el[1]#extract ip count
+
+			intiplist=ip.split(".")#convert ip address from dotted decimal to integer
+	
 
 			a=int(intiplist[0])*256**3
 			b=int(intiplist[1])*256**2
@@ -31,12 +29,8 @@ def maptoloc():
 			d=int(intiplist[3])*1
 			intip=int(a+b+c+d)
 		
-#print intip
-#print type(intip)
-#for el in cur.execute("SELECT ipaddress1,ipaddress2 from ipdb WHERE country_code LIKE 'IN%"):
-	#print el
 
-			r=cur.execute("SELECT * FROM ipdb WHERE int_ip1<=? AND int_ip2>?",(intip,intip,)).fetchall()
+			r=cur.execute("SELECT * FROM ipdb WHERE int_ip1<=? AND int_ip2>?",(intip,intip,)).fetchall()#select the information of all users which are located in the certain location based on their ipranges
 			ipmap=map(list,r)
 			if(len(ipmap)==0):
 			
