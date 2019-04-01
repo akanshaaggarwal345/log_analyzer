095e929ce693a51e2c5bc6079fe489f04e97225e
akanshaaggarwal345 <36618578+akanshaaggarwal345@users.noreply.github.com>
2019-04-01 15:30:26 +0530

    Update pushiplocation.py

--- pushiplocation.py
+++ pushiplocation.py
@@ -1,3 +1,5 @@
+#push final table to dbip database and create a help table which provide a list of country codes.
+#iploc is the final table which will be queried.
 import sqlite3,csv
 def pushloc():
 	flag=0
@@ -6,15 +8,15 @@ def pushloc():
 	cur=con.cursor()
 	cur.execute("DROP TABLE IF EXISTS help")
 	cur.execute("DROP TABLE IF EXISTS iploc")
-	cur.execute("CREATE TABLE iploc(ipaddress text,count int,country_code text,city text,region text)")
-	cur.execute("CREATE TABLE help(country_code text,country_name text)")
-	f4=open("countrycodes.csv","r")
+	cur.execute("CREATE TABLE iploc(ipaddress text,count int,country_code text,city text,region text)")#create new table called iploc
+	cur.execute("CREATE TABLE help(country_code text,country_name text)")#create a help table
+	f4=open("countrycodes.csv","r")#open csv file countrycodes.csv
 	reader4=csv.reader(f4,delimiter=',')
 	for elem in reader4:
 	
 		country_code=elem[0]
 		country_name=elem[1]
-		cur.execute("INSERT INTO help(country_code,country_name) VALUES(?,?)",(country_code,country_name))
+		cur.execute("INSERT INTO help(country_code,country_name) VALUES(?,?)",(country_code,country_name))#push countrycodes.csv to help table
 
 	f1=open("ipcountandlocation.csv","r")
 	reader=csv.reader(f1,delimiter=',')
@@ -25,6 +27,8 @@ def pushloc():
 		city=element[3]
 		region=element[4]
 		cur.execute("INSERT INTO iploc(ipaddress,count,country_code,city,region) VALUES(?,?,?,?,?)",(ipaddress1,count,country_code,city,region))
+		#insert the ipcountandlocation entire data into iploc table in sqlite3
+		#iploc is the table which can be finally queried to know about ipaddresses,their counts,location
 	f1.close()
 	f4.close()
 	con.commit()
