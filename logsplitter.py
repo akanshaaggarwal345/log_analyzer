cd98c2efd3e5c0b2bf1bb788b74cb36d1c94c859
akanshaaggarwal345 <36618578+akanshaaggarwal345@users.noreply.github.com>
2019-04-01 15:34:23 +0530

    Update logsplitter.py

--- logsplitter.py
+++ logsplitter.py
@@ -1,5 +1,6 @@
-#Here we are going to parse our log files and save them in a csv file named as log_contents.csv.The log file we have is an apache logfile which is using combined log format 
-#Log Format "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\""
+#Here we are going to parse our log files and save them in a csv file named as log_contents.csv.
+#The log file we have is an apache logfile in combined log format 
+#Log Format: "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\""
 # %h: IP Address
 # %l: remote logname
 # %u:user id of the requester
@@ -9,20 +10,24 @@
 # %b: size of the object returned to the client.
 # %{Refer}i: This gives the site that the client reports having been referred from
 # %{User-agent}i: The User-Agent HTTP request header. This is the identifying information that the client browser reports about itself.
-# So the log is going to look like :199.30.24.222 - - [31/May/2017:01:17:49 +0530] "GET /images/img2.jpg HTTP/1.1" 200 16276 "-" "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53 BingPreview/1.0b"
+# So the log is going to look like :
+#199.30.24.222 - - [31/May/2017:01:17:49 +0530] "GET /images/img2.jpg HTTP/1.1" 200 16276 "-"
+#"Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) 
+#Version/7.0 Mobile/11A465 Safari/9537.53 BingPreview/1.0b"
 import glob 
 import re
 
 file2=open("log_contents.csv","w") # creating the new file
-#defining splitcount functions which is going to split  the contents of log file
+#defining splitcount functions to split  the contents of log file
 def splitcontent(logfiles):
 	file1=open(logfiles,"r")#apache combined log format
 	file2.write("IP ADDRESS,DATE AND TIME,METHOD AND PROTOCOL,STATUS,SIZE\n") # header of our file
 	for line in file1:
-		list1=line.split("]")[0].split("[")[0].split(" ") # split till date first and then split based on " " and store the contents in a list
+		list1=line.split("]")[0].split("[")[0].split(" ")
+		# split till date first and then split based on " " and store the contents in a list
 		del(list1[3])
-    
-		ipaddress=list1[0] # extract the useful information from the list
+    		# extract the useful information from the list
+		ipaddress=list1[0] 
 		uid=list1[1]
 		username=list1[2]
 		list2=line.split("]")[0].split("[")
