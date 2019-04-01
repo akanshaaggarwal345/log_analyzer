be7a2fae728a727227995e10c11fe52c5a3ad119
akanshaaggarwal345 <36618578+akanshaaggarwal345@users.noreply.github.com>
2019-04-01 13:21:03 +0530

    Update logsplitter.py

--- logsplitter.py
+++ logsplitter.py
@@ -1,16 +1,28 @@
-import glob
+#Here we are going to parse our log files and save them in a csv file named as log_contents.csv.The log file we have is an apache logfile which is using combined log format 
+#Log Format "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\""
+# %h: IP Address
+# %l: remote logname
+# %u:user id of the requester
+# %t: Date and Time
+# %r: It represents three important information method,resource accessed,protocol used
+# %>s: This is status code that server sends back to the client
+# %b: size of the object returned to the client.
+# %{Refer}i: This gives the site that the client reports having been referred from
+# %{User-agent}i: The User-Agent HTTP request header. This is the identifying information that the client browser reports about itself.
+# So the log is going to look like :199.30.24.222 - - [31/May/2017:01:17:49 +0530] "GET /images/img2.jpg HTTP/1.1" 200 16276 "-" "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53 BingPreview/1.0b"
+import glob 
 import re
 
-file2=open("log_contents.csv","w")
-
+file2=open("log_contents.csv","w") # creating the new file
+#defining splitcount functions which is going to split  the contents of log file
 def splitcontent(logfiles):
 	file1=open(logfiles,"r")#apache combined log format
-	file2.write("IP ADDRESS,DATE AND TIME,METHOD AND PROTOCOL,STATUS,SIZE\n")
+	file2.write("IP ADDRESS,DATE AND TIME,METHOD AND PROTOCOL,STATUS,SIZE\n") # header of our file
 	for line in file1:
-		list1=line.split("]")[0].split("[")[0].split(" ")
+		list1=line.split("]")[0].split("[")[0].split(" ") # split till date first and then split based on " " and store the contents in a list
 		del(list1[3])
     
-		ipaddress=list1[0]
+		ipaddress=list1[0] # extract the useful information from the list
 		uid=list1[1]
 		username=list1[2]
 		list2=line.split("]")[0].split("[")
@@ -33,11 +45,7 @@ def splitcontent(logfiles):
 			status=list4[0]
 			size=list4[1]
 		method=list3[1]
-		#statusandsize=list3[2]
-		#status=statusandsize[0]
-		#size=statusandsize[1]
-		#referenceaddress=list3[4]
-		#useragent=list3[5]
+		
    
 
 		file2.write(ipaddress+ "," +date+","+method+","+status+","+size+"\n")
