6ff6c8092e5cc3371dcb695a99be10dadd8e2709
akanshaaggarwal345 <36618578+akanshaaggarwal345@users.noreply.github.com>
2019-04-01 13:57:59 +0530

    Update createdbip.py

--- createdbip.py
+++ createdbip.py
@@ -1,11 +1,12 @@
+#CREATING DBIP DATABASE,WHERE IP ADDRESES AND LOCATIONS ARE STORED IN IPDB TABLE FROM iprange.csv file 
 import sqlite3,csv
 def pushtodb():
-	con=sqlite3.connect("dbip.db")
+	con=sqlite3.connect("dbip.db")#CONNECTING TO SQLITE3 AND CREATING THE DATABASE
 	con.text_factory=str
 	cur=con.cursor()
 	list1=[]
 	f=open("iprange.csv","rU")
-	reader=csv.reader(f,delimiter=',')
+	reader=csv.reader(f,delimiter=',') #READ iprange.csv file
 	cur.execute("DROP TABLE IF EXISTS ipdb")
 	cur.execute("DROP TABLE IF EXISTS cache_table")
 	cur.execute("CREATE TABLE ipdb(ipaddress1 text,int_ip1 int,ipaddress2 text,int_ip2 int,country_code text,city text,state text)")
